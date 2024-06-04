f54c227e547cfd5a8097d8b69f4124c2a3b2dacf
Ly Lam <llam@fairventureslab.ca>
2024-06-04 01:53:15 -0400

    Started UI for adding guest player
    Add a common upsert vue for guests.
    Reuse the waiting column for the guest input controls rather than using a dialog.

---
+++ vueapp/src/components/GuestUpsert.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card
+    max-width="400"
+    prepend-icon="mdi-account-box-outline"
+    text="Blah blah blah"
+    title="Guest"
+  >
+    <div>
+      <v-text-field label="Name"></v-text-field>
+      <v-select
+        label="Skill level"
+        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+      ></v-select>
+    </div>
+    <template v-slot:actions>
+      <v-btn class="ms-auto" text="Ok" @click="save"></v-btn>
+    </template>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["close"]);
+const props = defineProps(["player"]);
+
+function save() {
+  emit("close");
+}
+</script>
