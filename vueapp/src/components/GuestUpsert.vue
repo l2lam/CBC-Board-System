729cbdbf315b64ca3f631d2462c326b496323580
Ly Lam <llam@fairventureslab.ca>
2024-06-04 02:18:33 -0400

    Started binding values in guest upsert screen

--- vueapp/src/components/GuestUpsert.vue
+++ vueapp/src/components/GuestUpsert.vue
@@ -3,24 +3,29 @@
     max-width="400"
     prepend-icon="mdi-account-box-outline"
     text="Blah blah blah"
-    title="Guest"
+    :title="isNewPlayer ? 'New Guest' : 'Edit Guest'"
   >
     <div>
-      <v-text-field label="Name"></v-text-field>
+      <v-text-field label="Name" v-model="guest.name"></v-text-field>
       <v-select
         label="Skill level"
         :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
       ></v-select>
     </div>
     <template v-slot:actions>
-      <v-btn class="ms-auto" text="Ok" @click="save"></v-btn>
+      <v-btn class="ms-auto" :text="isNewPlayer ? 'ADD' : 'SAVE'" @click="save"></v-btn>
     </template>
   </v-card>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from "vue";
+import { Player } from "../models/player";
+
 const emit = defineEmits(["close"]);
 const props = defineProps(["player"]);
+const isNewPlayer = computed(() => (props.player ? false : true));
+const guest = isNewPlayer.value ? ref(new Player("Test")) : props.player;
 
 function save() {
   emit("close");
