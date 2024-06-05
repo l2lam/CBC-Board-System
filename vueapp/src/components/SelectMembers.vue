3fd9edcc59878e81782f01d9d9429560a21f63b5
Jackson Dahl <jdahl087@gmail.com>
2024-06-05 11:35:05 -0400

    Added checkboxs to select members

--- vueapp/src/components/SelectMembers.vue
+++ vueapp/src/components/SelectMembers.vue
@@ -1,20 +1,32 @@
 <template>
+
   <v-container fluid fill-height class="d-flex flex-column" style="height: 90%">
-      Waiting List
+      Select members to add
       <v-list>
         <v-list-item
-          v-for="player in store.waitingPlayers"
-          @click="playerSelected(player)"
+          v-for="player in store.allPlayers"
         >
-          <template v-slot:prepend>
-            <v-icon
-              :icon="player.isGuest ? 'mdi-account-box-outline' : 'mdi-account-circle'"
-            ></v-icon>
-          </template>
-          {{ player.name }}</v-list-item
-        >
+          <v-checkbox>  
+            <template v-slot:prepend>
+              <v-icon
+                :icon="player.isGuest ? 'mdi-account-box-outline' : 'mdi-account-circle'"
+              ></v-icon>
+            </template>
+            {{ player.name }}
+          </v-checkbox>
+        </v-list-item>
       </v-list>
+      <v-btn
+          height="72"
+          min-width="140"
+          prepend-icon="mdi-account-circle"
+          :stacked="true"
+          @click="addMemberPlayers"
+        >
+         Done
+        </v-btn>
     </v-container>
+    
 </template>
 
 <script setup lang="ts">
@@ -24,4 +36,9 @@ import { usePlayerStore } from "@/stores/playerStore";
 const store = usePlayerStore();
 const currentScreen = ref(Screen.WAITING);
 const currentPlayer = ref(null);
+
+function addSelectedMembers() {
+
+}
+
 </script>
