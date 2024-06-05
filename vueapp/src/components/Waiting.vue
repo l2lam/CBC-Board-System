d42324935549b1b3b7ee35f2dcd79cebdc16b57f
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-06-05 12:05:37 -0400

    Fix #13

--- vueapp/src/components/Waiting.vue
+++ vueapp/src/components/Waiting.vue
@@ -1,16 +1,18 @@
 <template>
   <v-container fluid fill-height class="d-flex flex-column">
     Waiting List
-    <v-list>
-      <v-list-item v-for="player in store.waitingPlayers">{{
-        player.name
-      }}</v-list-item>
-    </v-list>
+    <draggable :list="store.waitingPlayers">
+      <template #item="{ element }">
+        <v-list-item :title="element.name">
+        </v-list-item>
+      </template>
+    </draggable>
   </v-container>
 </template>
 
 <script setup lang="ts">
 import { usePlayerStore } from "@/stores/playerStore";
+import draggable from "vuedraggable";
 
 const store = usePlayerStore();
 </script>
