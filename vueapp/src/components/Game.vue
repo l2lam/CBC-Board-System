2b3d8ba7bda0589318dbf33ded6d9f61a47df6e7
Ly Lam <lylam99@gmail.com>
2024-06-06 16:10:55 -0400

    Remove transition experiment

--- vueapp/src/components/Game.vue
+++ vueapp/src/components/Game.vue
@@ -1,49 +1,47 @@
 <template>
-  <transition name="flip">
-    <v-card
-      class="mx-auto w-100 ma-2 bg-surface-light"
-      variant="elevated"
-      :color="flipped ? 'primary' : 'secondary'"
+  <v-card
+    class="mx-auto w-100 ma-2 bg-surface-light"
+    variant="elevated"
+    :color="flipped ? 'primary' : 'secondary'"
+  >
+    <v-card-text
+      v-if="!flipped"
+      class="bg-surface-light"
+      @click="flipped = !flipped"
     >
-      <v-card-text
-        v-if="!flipped"
-        class="bg-surface-light"
-        @click="flipped = !flipped"
-      >
-        <v-list class="bg-surface-light">
-          <v-list-item v-for="player in game.players">
-            <template v-slot:prepend>
-              <v-icon
-                :icon="
-                  player.isGuest
-                    ? 'mdi-account-box-outline'
-                    : 'mdi-account-circle'
-                "
-              ></v-icon>
-            </template>
-            <v-list-item-title>
-              {{ player.name }}
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
+      <v-list class="bg-surface-light">
+        <v-list-item v-for="player in game.players">
+          <template v-slot:prepend>
+            <v-icon
+              :icon="
+                player.isGuest
+                  ? 'mdi-account-box-outline'
+                  : 'mdi-account-circle'
+              "
+            ></v-icon>
+          </template>
+          <v-list-item-title>
+            {{ player.name }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+    <div v-else>
+      <v-card-text>
+        <v-select
+          label="Play"
+          :items="['TODO - Court 1', 'TODO - Court 2']"
+        ></v-select>
       </v-card-text>
-      <div v-else>
-        <v-card-text>
-          <v-select
-            label="Play"
-            :items="['TODO - Court 1', 'TODO - Court 2']"
-          ></v-select>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            text="REMOVE"
-            @click="gameStore.removeGameAt(props.gameIndex)"
-          ></v-btn>
-          <v-btn text="BACK" @click="flipped = !flipped"></v-btn>
-        </v-card-actions>
-      </div>
-    </v-card>
-  </transition>
+      <v-card-actions>
+        <v-btn
+          text="REMOVE"
+          @click="gameStore.removeGameAt(props.gameIndex)"
+        ></v-btn>
+        <v-btn text="BACK" @click="flipped = !flipped"></v-btn>
+      </v-card-actions>
+    </div>
+  </v-card>
 </template>
 
 <script setup lang="ts">
