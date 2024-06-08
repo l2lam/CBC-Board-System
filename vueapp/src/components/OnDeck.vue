92f6252bf66569bdd2fceaa390b499fb14affaf8
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-06-08 17:35:57 -0400

    Allow games to be rearranged

--- vueapp/src/components/OnDeck.vue
+++ vueapp/src/components/OnDeck.vue
@@ -2,12 +2,20 @@
   <v-sheet class="pa-4 mx-auto" max-width="600" width="100%" height="100%">
     <v-container fluid fill-height class="d-flex flex-column" style="height: 90%">
       <p class="text-h6 text-center">On-deck</p>
-      <Game
-        v-for="(game, index) in gameStore.gamesOnDeck"
-        :key="game.id"
-        :game="game"
-        :gameIndex="index"
-      ></Game>
+      <draggable
+        :list="gameStore.gamesOnDeck"
+        item-key="id"
+        v-bind="dragOptions"
+        @start="isDragging = true"
+        @end="isDragging = false"
+      >
+        <template #item="{ element, index }">
+          <Game
+            :game="element"
+            :gameIndex="index"
+          ></Game>
+        </template>
+      </draggable>
     </v-container>
   </v-sheet>
 </template>
@@ -19,6 +27,10 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useGameStore } from "../stores/gameStore";
+import draggable from "vuedraggable";
 
 const gameStore = useGameStore();
+const dragOptions = {
+  animation: 100,
+}
 </script>
