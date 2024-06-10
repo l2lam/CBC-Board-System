6c71ec0be9334fb191c5d76e5db02c94ea17ded4
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-06-10 11:31:05 -0400

    Improve appearance of lists

--- vueapp/src/components/OnDeck.vue
+++ vueapp/src/components/OnDeck.vue
@@ -1,7 +1,7 @@
 <template>
   <v-sheet class="pa-4 mx-auto" max-width="600" width="100%" height="100%">
     <v-container fluid fill-height class="d-flex flex-column" style="height: 90%">
-      <p class="text-h6 text-center">On-deck</p>
+      <p class="text-h6 text-left mb-4">On Deck</p>
       <draggable
         :list="gameStore.gamesOnDeck"
         item-key="id"
@@ -10,10 +10,12 @@
         @end="isDragging = false"
       >
         <template #item="{ element, index }">
-          <Game
-            :game="element"
-            :gameIndex="index"
-          ></Game>
+          <div class="d-flex mb-3">
+            <Game
+              :game="element"
+              :gameIndex="index"
+            ></Game>
+          </div>
         </template>
       </draggable>
     </v-container>
