bc0cc47ce1a3b9ea8a7465bfd64cf4d11a538a14
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-06-10 11:31:32 -0400

    Display on deck games in a list

--- vueapp/src/components/OnDeck.vue
+++ vueapp/src/components/OnDeck.vue
@@ -2,22 +2,24 @@
   <v-sheet class="pa-4 mx-auto" max-width="600" width="100%" height="100%">
     <v-container fluid fill-height class="d-flex flex-column" style="height: 90%">
       <p class="text-h6 text-left mb-4">On Deck</p>
-      <draggable
-        :list="gameStore.gamesOnDeck"
-        item-key="id"
-        v-bind="dragOptions"
-        @start="isDragging = true"
-        @end="isDragging = false"
-      >
-        <template #item="{ element, index }">
-          <div class="d-flex mb-3">
-            <Game
-              :game="element"
-              :gameIndex="index"
-            ></Game>
-          </div>
-        </template>
-      </draggable>
+      <v-list>
+        <draggable
+          :list="gameStore.gamesOnDeck"
+          item-key="id"
+          v-bind="dragOptions"
+          @start="isDragging = true"
+          @end="isDragging = false"
+        >
+          <template #item="{ element, index }">
+            <div class="d-flex mb-3">
+              <Game
+                :game="element"
+                :gameIndex="index"
+              ></Game>
+            </div>
+          </template>
+        </draggable>
+      </v-list>
     </v-container>
   </v-sheet>
 </template>
