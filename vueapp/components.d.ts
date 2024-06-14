8bbc968e90d043beb97b1f610018d03b721ca215
Ly Lam <lylam99@gmail.com>
2024-06-14 10:44:57 -0400

    Moved "court" to its own component
    Renamed "Courts" vue to "Venue" to distinguish from the new "Court" vue.

--- vueapp/components.d.ts
+++ vueapp/components.d.ts
@@ -9,13 +9,16 @@ declare module 'vue' {
   export interface GlobalComponents {
     Account: typeof import('./src/components/Account.vue')['default']
     Auth: typeof import('./src/components/Auth.vue')['default']
+    Court: typeof import('./src/components/Court.vue')['default']
     Courts: typeof import('./src/components/Courts.vue')['default']
+    CreateGame: typeof import('./src/components/CreateGame.vue')['default']
     Game: typeof import('./src/components/Game.vue')['default']
     GuestUpsert: typeof import('./src/components/GuestUpsert.vue')['default']
     MemberUpsert: typeof import('./src/components/MemberUpsert.vue')['default']
     OnDeck: typeof import('./src/components/OnDeck.vue')['default']
     Playing: typeof import('./src/components/Playing.vue')['default']
     SelectMembers: typeof import('./src/components/SelectMembers.vue')['default']
+    Venue: typeof import('./src/components/Venue.vue')['default']
     Waiting: typeof import('./src/components/Waiting.vue')['default']
   }
 }
---
+++ vueapp/src/components/Court.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- A court with a game on it -->
+    <Game
+      v-if="court && court.game"
+      :game="court.game"
+      :court="court"
+      class="courtcard"
+    ></Game>
+    <!-- A court with no game playing on it -->
+    <v-card v-else-if="court" class="pa-2 ma-2 court-card">
+      <v-card-title>{{ court.name }}</v-card-title>
+      <v-card-subtitle>Available</v-card-subtitle>
+    </v-card>
+    <!-- Empty space (no court) in the grid -->
+    <v-card v-else class="pa-2 ma-2 court-card"> </v-card>
+  </div>
+</template>
+
+<style>
+@import "@/assets/styles/custom.css";
+</style>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps(["court"]);
+const court = ref(props.court);
+</script>
