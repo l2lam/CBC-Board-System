36648a38632b59a8372f08c518d51084fb86f78d
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-06-16 13:16:10 -0400

    Fix Player level indicator #48

--- vueapp/src/components/Player.vue
+++ vueapp/src/components/Player.vue
@@ -3,6 +3,8 @@
   <template v-slot:prepend>
     <v-icon
       :icon="player.isGuest ? 'mdi-account-box-outline' : 'mdi-account-circle'"
+      :color="player?.level?.color"
+      v-tooltip="(player.isGuest ? 'Member (' : 'Guest (') + (player?.level?.name || 'No') + ' Level)'"
     ></v-icon>
   </template>
   <v-list-item-title>
