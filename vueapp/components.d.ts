3bb890effb4e7e3c9954e2b343d224c3c98ffc1b
Ly Lam <lylam99@gmail.com>
2024-06-17 10:40:05 -0400

    Add dividing line under the column titles.

--- vueapp/components.d.ts
+++ vueapp/components.d.ts
@@ -5,21 +5,21 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 export {}
 
-declare module "vue" {
+declare module 'vue' {
   export interface GlobalComponents {
-    Account: typeof import("./src/components/Account.vue")["default"];
-    Auth: typeof import("./src/components/Auth.vue")["default"];
-    Court: typeof import("./src/components/Court.vue")["default"];
-    CreateGame: typeof import("./src/components/CreateGame.vue")["default"];
-    Game: typeof import("./src/components/Game.vue")["default"];
-    GuestUpsert: typeof import("./src/components/GuestUpsert.vue")["default"];
-    MemberUpsert: typeof import("./src/components/MemberUpsert.vue")["default"];
-    OnDeck: typeof import("./src/components/OnDeck.vue")["default"];
-    Player: typeof import("./src/components/Player.vue")["default"];
-    Playing: typeof import("./src/components/Playing.vue")["default"];
-    QueueColumn: typeof import("./src/components/QueueColumn.vue")["default"];
-    SelectMembers: typeof import("./src/components/SelectMembers.vue")["default"];
-    Venue: typeof import("./src/components/Venue.vue")["default"];
-    Waiting: typeof import("./src/components/Waiting.vue")["default"];
+    Account: typeof import('./src/components/Account.vue')['default']
+    Auth: typeof import('./src/components/Auth.vue')['default']
+    Court: typeof import('./src/components/Court.vue')['default']
+    CreateGame: typeof import('./src/components/CreateGame.vue')['default']
+    Game: typeof import('./src/components/Game.vue')['default']
+    GuestUpsert: typeof import('./src/components/GuestUpsert.vue')['default']
+    MemberUpsert: typeof import('./src/components/MemberUpsert.vue')['default']
+    OnDeck: typeof import('./src/components/OnDeck.vue')['default']
+    Player: typeof import('./src/components/Player.vue')['default']
+    Playing: typeof import('./src/components/Playing.vue')['default']
+    QueueColumn: typeof import('./src/components/QueueColumn.vue')['default']
+    SelectMembers: typeof import('./src/components/SelectMembers.vue')['default']
+    Venue: typeof import('./src/components/Venue.vue')['default']
+    Waiting: typeof import('./src/components/Waiting.vue')['default']
   }
 }
