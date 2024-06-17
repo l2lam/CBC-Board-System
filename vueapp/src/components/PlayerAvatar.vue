7890747f7080ac53682028131bcbd5513be0b3ec
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-06-17 11:30:26 -0400

    Generate tooltip message more clearly

--- vueapp/src/components/PlayerAvatar.vue
+++ vueapp/src/components/PlayerAvatar.vue
@@ -1,15 +1,20 @@
 <template>
   <v-icon
     :icon="player.isGuest ? 'mdi-account-box-outline' : 'mdi-account-circle'"
-    :color="player?.level?.color"
-    v-tooltip="(player.isGuest ? 'Guest (' : 'Member (') + (player?.level?.name || 'No Level') + ')'"
+    :color="player.level?.color"
+    v-tooltip="getTooltip()"
   ></v-icon>
 </template>
 
 <script setup lang="ts">
 import { Player } from '../models/player';
 
-defineProps<{
+const props = defineProps<{
   player: Player
 }>();
+
+function getTooltip() {
+  var levelName = props.player.level?.name || 'No Level';
+  return props.player.isGuest ? `Guest (${levelName})` : `Member (${levelName})`;
+}
 </script>
