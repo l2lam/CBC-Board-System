3e515a6a24d399f0d9dd84723efa4c5611b06fd1
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-06-17 10:55:05 -0400

    Format files

--- vueapp/src/components/MemberUpsert.vue
+++ vueapp/src/components/MemberUpsert.vue
@@ -1,41 +1,34 @@
 <template>
-    <v-card
-      text="Blah blah blah"
-      :title=member.name
-      class="mx-auto"
-      width="90%"
-    >
-      <template v-slot:prepend>
-        <PlayerAvatar :player />
-      </template>
-      <v-card-actions>
-        <v-btn 
-        text="Initiate challenge"
-        @click="initiateChallenge"
-        ></v-btn>
-        <v-spacer></v-spacer>
-        <v-btn
-          text="DONE"
-          @click="done"
-        ></v-btn>
-      </v-card-actions>
-    </v-card>
-  </template>
-  
-  <script setup lang="ts">  
-  import { ref } from "vue";
-  import PlayerAvatar from "./PlayerAvatar.vue";
+  <v-card
+    text="Blah blah blah"
+    :title=member.name
+    class="mx-auto"
+    width="90%"
+  >
+    <template v-slot:prepend>
+      <PlayerAvatar :player />
+    </template>
+    <v-card-actions>
+      <v-btn text="Initiate challenge" @click="initiateChallenge"></v-btn>
+      <v-spacer></v-spacer>
+      <v-btn text="DONE" @click="done"></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
 
-  const props = defineProps(["player"]);
-  const member =  ref(props.player);
-  const emit = defineEmits(["close"]);
+<script setup lang="ts">
+import { ref } from "vue";
+import PlayerAvatar from "./PlayerAvatar.vue";
 
-  function initiateChallenge() {
-    // will initiate a challenge
-  }
-  
-  function done() {
-    emit("close");
-  }
-  </script>
-  
+const props = defineProps(["player"]);
+const member = ref(props.player);
+const emit = defineEmits(["close"]);
+
+function initiateChallenge() {
+  // will initiate a challenge
+}
+
+function done() {
+  emit("close");
+}
+</script>
--- vueapp/src/components/Player.vue
+++ vueapp/src/components/Player.vue
@@ -1,15 +1,15 @@
 <template>
-<v-list-item>
-  <template v-slot:prepend>
-    <PlayerAvatar :player />
-  </template>
-  <v-list-item-title>
-    {{ player.name }}
-  </v-list-item-title>
-  <template v-slot:append>
-    <slot name="append"></slot>
-  </template>
-</v-list-item>
+  <v-list-item>
+    <template v-slot:prepend>
+      <PlayerAvatar :player />
+    </template>
+    <v-list-item-title>
+      {{ player.name }}
+    </v-list-item-title>
+    <template v-slot:append>
+      <slot name="append"></slot>
+    </template>
+  </v-list-item>
 </template>
 
 <script setup lang="ts">
