0dd8f6e22a2bacd1ffb83a4051fc84b3044813e1
Ly Lam <llam@fairventureslab.ca>
2024-07-11 02:58:03 -0400

    Make member upsert/challenge component uniform with queue column style

--- vueapp/src/components/MemberUpsert.vue
+++ vueapp/src/components/MemberUpsert.vue
@@ -1,124 +1,112 @@
 <template>
-  <v-card
-    v-if="currentScreen == Screen.INITIALSCREEN"
-    text="Blah blah blah"
-    :title="challenger.name"
-    class="mx-auto"
-    width="90%"
-  >
-    <template v-slot:prepend>
-      <PlayerAvatar :player />
+  <QueueColumn>
+    <template v-slot:main>
+      <v-card :title="member.name" class="mx-auto" width="90%">
+        <template v-slot:prepend>
+          <PlayerAvatar :player="member" />
+        </template>
+        <div v-if="currentScreen == Screen.CHALLENGE">
+          <!-- <v-card-subtitle>Initiate a Challenge</v-card-subtitle> -->
+          <v-card-text>Initiate a Challenge</v-card-text>
+          <v-select
+            label="Target level"
+            :items="levelStore.allLevels"
+            item-title="name"
+            v-model="targetLevel"
+            return-object
+          >
+          </v-select>
+          <div v-if="targetLevel">
+            <p class="text-h6">Select Players for the challenge</p>
+            <v-list>
+              <Player
+                :player="incumbent"
+                v-for="incumbent in incumbents"
+                :key="incumbent.name"
+              >
+                <template v-slot:append>
+                  <v-checkbox-btn
+                    v-model="selectedIncumbents"
+                    :value="incumbent"
+                  ></v-checkbox-btn>
+                </template>
+              </Player>
+            </v-list>
+          </div>
+        </div>
+      </v-card>
     </template>
-    <v-card-actions>
-      <v-btn text="Initiate challenge" @click="goToChallengeSetUp"></v-btn>
-      <v-spacer></v-spacer>
-      <v-btn text="DONE" @click="done"></v-btn>
-    </v-card-actions>
-  </v-card>
-  <v-card
-    v-if="currentScreen == Screen.SETUPSCREEN"
-    prepend-icon="mdi-account-circle"
-    text="Blah blah blah"
-    :title="challenger.name"
-    class="mx-auto"
-    width="90%"
-  >
-    <div>
-      <v-select
-        label="Target level"
-        :items="levelStore.allLevels"
-        item-title="name"
-        v-model="targetLevel"
-      >
-      </v-select>
-    </div>
-    <v-card-actions>
-      <v-btn
-        stacked="true"
-        text="Select challengers"
-        @click="goToChallengerSelect"
-      ></v-btn>
-    </v-card-actions>
-  </v-card>
-  <v-sheet
-    v-else-if="currentScreen == Screen.SELECTCHALLENGERS"
-    class="pa-4 mx-auto"
-    max-width="600"
-    width="100%"
-    height="100%"
-  >
-    <QueueColumn>
-      <template v-slot:main>
-        <p class="text-h6">Select Players for Game</p>
-        <v-list>
-          <Player :player="member" v-for="member in challengers" :key="member.name">
-            <template v-slot:append>
-              <v-checkbox-btn
-                v-model="selectedIncumbents"
-                :value="member"
-              ></v-checkbox-btn>
-            </template>
-          </Player>
-        </v-list>
-      </template>
-      <template v-slot:actions>
-        <v-btn prepend-icon="mdi-check" :stacked="true" @click="createChallenge">
-          Done
+    <template v-slot:actions>
+      <div v-if="currentScreen == Screen.CHALLENGE">
+        <v-btn
+          prepend-icon="mdi-cancel"
+          :stacked="true"
+          @click="currentScreen = Screen.MAIN"
+        >
+          Cancel
         </v-btn>
-      </template>
-    </QueueColumn>
-  </v-sheet>
+        <v-btn
+          v-if="targetLevel && selectedIncumbents.length > 0"
+          prepend-icon="mdi-one-up"
+          stacked
+          @click="createChallenge"
+        >
+          Go
+        </v-btn>
+      </div>
+      <div v-else-if="currentScreen == Screen.MAIN">
+        <v-btn
+          prepend-icon="mdi-one-up"
+          text="Initiate challenge"
+          stacked
+          @click="goToChallengeSetUp"
+        ></v-btn>
+        <v-btn prepend-icon="mdi-check" stacked @click="done"> Done </v-btn>
+      </div>
+    </template>
+  </QueueColumn>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { useLevelStore } from "../stores/levelStore";
 import { usePlayerStore } from "../stores/playerStore";
 import { useChallengeStore } from "../stores/challengeStore";
 import { useGameStore } from "../stores/gameStore";
 import { Game } from "../models/game";
+import { Member } from "../models/player";
 
 enum Screen {
-  INITIALSCREEN,
-  SETUPSCREEN,
-  SELECTCHALLENGERS,
+  MAIN,
+  CHALLENGE,
 }
 
-const currentScreen = ref(Screen.INITIALSCREEN);
-const props = defineProps(["player"]);
-const challenger = ref(props.player);
+const currentScreen = ref(Screen.MAIN);
+const props = defineProps({ member: Member });
 const levelStore = useLevelStore();
 const playerStore = usePlayerStore();
 const challengeStore = useChallengeStore();
 const gameStore = useGameStore();
 const emit = defineEmits(["close"]);
-const targetLevel = ref(challenger.value.level);
-const challengers = ref([]);
+const targetLevel = ref();
+const incumbents = computed(() => {
+  selectedIncumbents.value = []; // Reset incumbents as a side-effect of changing target levels
+  return playerStore.allMembers.filter((m) => m.level == targetLevel.value);
+});
 const selectedIncumbents = ref([]);
 
 function goToChallengeSetUp() {
-  currentScreen.value = Screen.SETUPSCREEN;
-}
-
-function goToChallengerSelect() {
-  // Creates array of members with target level
-  playerStore.allMembers.forEach((member) => {
-    if (targetLevel.value == member.level) {
-      challengers.value.push(member);
-    }
-  });
-  currentScreen.value = Screen.SELECTCHALLENGERS;
+  currentScreen.value = Screen.CHALLENGE;
 }
 
 async function createChallenge() {
   const challenge = await challengeStore.registerNewChallenge(
-    challenger.value,
+    props.member,
     targetLevel.value,
     selectedIncumbents.value
   );
-  selectedIncumbents.value.push(challenger);
   gameStore.addGameToOnDeckQueue(
-    new Game([challenger, ...selectedIncumbents.value], challenge)
+    new Game([props.member, ...selectedIncumbents.value], challenge)
   );
   done();
 }
