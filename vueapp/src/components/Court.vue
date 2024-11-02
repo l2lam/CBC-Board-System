bc7153113ef471f1dbeebd7ebc8e0ba5e9c4a18c
Ly Lam <llam@fairventureslab.ca>
2024-11-02 18:03:11 -0400

    Revert "Try using native draggable attribute instead of vuedraggable to test if touchscreen dropping works"
    This reverts commit 88509fc9230ba61e9972cb9de5ed0488c48700ab.

--- vueapp/src/components/Court.vue
+++ vueapp/src/components/Court.vue
@@ -1,26 +1,30 @@
 <template>
-  <div
-    draggable="true"
-    @dragenter="dragEnter"
-    @dragover="dragEnter"
-    @dragstart="drag"
+  <draggable
+    v-model="courts"
+    item-key="id"
+    v-bind="dragOptions"
     @drop="drop"
+    @dragstart="drag"
   >
-    <!-- A court with a game on it -->
-    <Game
-      v-if="court && court.game"
-      :game="court.game"
-      :court="court"
-      class="pa-2, ma-2 court-card"
-    ></Game>
-    <!-- A court with no game playing on it -->
-    <v-card v-else-if="court" class="pa-2 ma-2 court-card">
-      <v-card-title>{{ court.name }}</v-card-title>
-      <v-card-subtitle>Available</v-card-subtitle>
-    </v-card>
-    <!-- Empty space (no court) in the grid -->
-    <v-card v-else class="pa-2 ma-2 court-card" @drop="drop"> </v-card>
-  </div>
+    <template #item="{ element: court }">
+      <div>
+        <!-- A court with a game on it -->
+        <Game
+          v-if="court && court.game"
+          :game="court.game"
+          :court="court"
+          class="pa-2, ma-2 court-card"
+        ></Game>
+        <!-- A court with no game playing on it -->
+        <v-card v-else-if="court" class="pa-2 ma-2 court-card">
+          <v-card-title>{{ court.name }}</v-card-title>
+          <v-card-subtitle>Available</v-card-subtitle>
+        </v-card>
+        <!-- Empty space (no court) in the grid -->
+        <v-card v-else class="pa-2 ma-2 court-card"> </v-card>
+      </div>
+    </template>
+  </draggable>
 </template>
 
 <style>
@@ -28,6 +32,7 @@
 </style>
 
 <script setup lang="ts">
+import draggable from "vuedraggable";
 import { ref } from "vue";
 import { useGameStore } from "../stores/gameStore";
 import { useCourtStore } from "../stores/courtStore";
@@ -41,10 +46,6 @@ const courtStore = useCourtStore();
 const dragOptions = {
   animation: 100,
 };
-
-function dragEnter(evt) {
-  evt.preventDefault();
-}
 
 // Handle court/game dragging event.  Communicate the data via the event's dataTransfer configuration
 function drag(evt) {
@@ -75,6 +76,5 @@ function drop(evt) {
       if (sourceCourt) sourceCourt.game = undefined;
     }
   }
-  evt.preventDefault();
 }
 </script>
